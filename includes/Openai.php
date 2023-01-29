5e023a25f0d1af5b514ce33e0bbec0173e6e9648
joemama155 <joemama155@gmail.com>
2023-01-29 00:18:05 -0500

    Update Openai.php
    removed the search function, openai api docs say it is deprecated
    changed the way it outputs the response to better fit my needs.

--- includes/Openai.php
+++ includes/Openai.php
@@ -5,7 +5,7 @@ class Openai{
     }
 
     public function request($engine, $prompt, $max_tokens){ 
-
+        $timestamp = date("c", strtotime("now"));
         $request_body = [
         "prompt" => $prompt,
         "max_tokens" => $max_tokens,
@@ -41,54 +41,22 @@ class Openai{
         curl_close($curl);
 
         if ($err) {
-            echo "Error #:" . $err;
+            $response="**Beep Boop Does Not Compute** ".$err." | **Bot Task:** ".$prompt." | **Time Stamp:** ".$timestamp;
         } else {
-            echo $response;
+            $response=json_decode($response,true);
+            if (isset($response['choices'][0]['text'])) {
+                $response=$response['choices'][0]['text'];
+            } else if (isset($response['error']['message'])){
+                $response="**Beep Boop Does Not Compute** ".$response['error']['message']." | **Bot Task:** ".$prompt." | **Time Stamp:** ".$timestamp;
+            } else {
+                $response=false;
+            }
+        
         }
+
+        return $response;
 
     }
 
-    public function search($engine, $documents, $query){ 
-
-        $request_body = [
-        "max_tokens" => 10,
-        "temperature" => 0.7,
-        "top_p" => 1,
-        "presence_penalty" => 0.75,
-        "frequency_penalty"=> 0.75,
-        "documents" => $documents,
-        "query" => $query
-        ];
-
-        $postfields = json_encode($request_body);
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-        CURLOPT_URL => "https://api.openai.com/v1/engines/" . $engine . "/search",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => $postfields,
-        CURLOPT_HTTPHEADER => [
-            'Content-Type: application/json',
-            'Authorization: ' . $this->secret_key()
-        ],
-        ]);
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-            echo "Error #:" . $err;
-        } else {
-            echo $response;
-        }
-
-    }
-
+  
 }
